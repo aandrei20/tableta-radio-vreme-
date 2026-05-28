3f9172ad03927b101d41ad59210611f8baa1a453
aandrei20 <pandreicristian0109@gmail.com>
2026-05-28 22:46:45 +0300

    PWA: manifest, service worker, iconita - instalabil ca aplicatie

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'vremea-radio-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // nu cacheaza API-ul de vreme si stream-ul radio - astea trebuie sa fie live
+  if (url.hostname.includes('open-meteo.com') || url.hostname.includes('srr.ro')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
